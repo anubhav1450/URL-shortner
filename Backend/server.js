373f143145012766f0fccf002fe92fd72eb4077d
Anubhav Kulshreshtha <124580419+anubhav1450@users.noreply.github.com>
2026-05-04 21:36:34 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const app = require("./src/app")
 const connectDB  = require("./src/db/db")
 const urlRouter = require('./src/routes/route.url')
-
+const port = process.env.Port || 3000;
 
 
 connectDB();
